7a0b45322023878a233e64a2426bf34128aef98f
Fabricio C Zuardi <fabricio@fabricio.org>
2024-11-23 11:58:07 -0300

    question 3 second try

--- submission/003.sh
+++ submission/003.sh
@@ -5,11 +5,11 @@
 height=123456
 
 
+#  "utxo_increase" : n,           (numeric, optional) The increase/decrease in the number of unspent outputs (not discounting op_return and similar)
 #  "utxo_size_inc" : n,           (numeric, optional) The increase/decrease in size for the utxo index (not discounting op_return and similar)
 #  "utxo_increase_actual" : n,    (numeric, optional) The increase/decrease in the number of unspent outputs, not counting unspendables
 #  "utxo_size_inc_actual" : n     (numeric, optional) The increase/decrease in size for the utxo index, not counting unspendables
 
+stats='["utxo_increase","utxo_size_inc","utxo_increase_actual","utxo_size_inc_actual"]'
 
-#  "utxo_increase" : n,           (numeric, optional) The increase/decrease in the number of unspent outputs (not discounting op_return and similar)
-stats="utxo_increase"
-bitcoin-cli getblockstats ${height} "[\"${stats}\"]" | jq .utxo_increase
+bitcoin-cli getblockstats ${height} ${stats} | jq .utxo_size_inc
