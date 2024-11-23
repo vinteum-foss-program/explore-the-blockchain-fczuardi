77e5617391acbda41c20ffb43d7e2859d061ccb6
Fabricio C Zuardi <fabricio@fabricio.org>
2024-11-23 19:49:32 -0300

    question 4

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,17 @@
+#!/bin/bash
+
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+
+# Extended public key
+xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
+
+# Subpath to derive
+subpath="100"
+
+descriptor_without_checksum="tr(${xpub}/${subpath})"
+
+descriptor=$(bitcoin-cli getdescriptorinfo "$descriptor_without_checksum" | jq -r '.descriptor')
+
+# Derive the address
+bitcoin-cli deriveaddresses "$descriptor" | jq -r .[0]
