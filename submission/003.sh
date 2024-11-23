3c3b0ce16722ce70a8b3701942b674e9cd6289fb
Fabricio C Zuardi <fabricio@fabricio.org>
2024-11-23 12:09:01 -0300

    trial and error (question 3)

--- submission/003.sh
+++ submission/003.sh
@@ -4,12 +4,13 @@
 
 height=123456
 
-
+#  "outs" : n,                    (numeric, optional) The number of outputs
 #  "utxo_increase" : n,           (numeric, optional) The increase/decrease in the number of unspent outputs (not discounting op_return and similar)
 #  "utxo_size_inc" : n,           (numeric, optional) The increase/decrease in size for the utxo index (not discounting op_return and similar)
 #  "utxo_increase_actual" : n,    (numeric, optional) The increase/decrease in the number of unspent outputs, not counting unspendables
 #  "utxo_size_inc_actual" : n     (numeric, optional) The increase/decrease in size for the utxo index, not counting unspendables
 
-stats='["utxo_increase","utxo_size_inc","utxo_increase_actual","utxo_size_inc_actual"]'
+# stats='["outs", "utxo_increase","utxo_size_inc","utxo_increase_actual","utxo_size_inc_actual"]'
+#bitcoin-cli getblockstats ${height} ${stats}
 
-bitcoin-cli getblockstats ${height} ${stats} | jq .utxo_size_inc
+bitcoin-cli getblockstats ${height} | jq .outs
